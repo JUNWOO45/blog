11cc5225ec78f395148542a5434138249a667d04
Parkjunwoo <sinyoorang@gmail.com>
2019-08-23 00:22:50 +0900

    meta config 추가 수정 및 이것저것 만져보기

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: `박준우 블로그`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `박준우`,
+  introduction: `기록을 남기는 것을 좋아하는 프론트엔드 개발자입니다.`,
+  siteUrl: `https://agitated-aryabhata-69cfdb.netlify.com/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: `github.com/junwoo45`, // Your GitHub account
+    github: `junwoo45`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
   },
@@ -14,13 +14,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'junwoo45/blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    // buyMeACoffeeId: 'jbee',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
